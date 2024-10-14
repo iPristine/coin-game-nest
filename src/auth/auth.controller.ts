266f4a69e3152a2f718a36430bb80cedb20bd865
iPristine <ipristine@ya.ru>
2024-10-14 20:57:14 +0300

    update

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,10 @@
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
-import * as crypto from 'crypto';
-import { ApiQuery, ApiBody } from '@nestjs/swagger';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiBody } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
-import { UserService } from '../user/user.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(
-    private readonly userService: UserService,
-    private readonly authService: AuthService,
-  ) {}
+  constructor(private readonly authService: AuthService) {}
 
   @Post('login')
   @ApiBody({
@@ -23,62 +18,5 @@ export class AuthController {
   })
   async login(@Body() body: { initData: string }) {
     return await this.authService.login(body.initData);
-  }
-
-  @Get('telegram')
-  @ApiQuery({
-    name: 'query_id',
-    required: true,
-    type: String,
-    description: 'Query ID',
-  })
-  @ApiQuery({
-    name: 'user',
-    required: true,
-    type: String,
-    description: 'User information in JSON format',
-  })
-  @ApiQuery({
-    name: 'auth_date',
-    required: true,
-    type: Number,
-    description: 'Authorization date',
-  })
-  @ApiQuery({ name: 'hash', required: true, type: String, description: 'Hash' })
-  async telegramAuth(
-    // @Query('query_id') queryId: string,
-    @Query('user') userJson: string,
-    // @Query('auth_date') authDate: number,
-    // @Query('hash') hash: string,
-  ) {
-    // Commented out because we don't need to check the hash in the DEV backend
-    // const token = process.env.TG_BOT_TOKEN;
-
-    const user = JSON.parse(decodeURIComponent(userJson));
-
-    // const dataCheckString = [
-    //   `query_id=${queryId}`,
-    //   `user=${userJson}`,
-    //   `auth_date=${authDate}`,
-    // ]
-    //   .sort()
-    //   .join('\n');
-    // const secretKey = crypto.createHash('sha256').update(token).digest();
-
-    // const computedHash = crypto
-    //   .createHmac('sha256', secretKey)
-    //   .update(dataCheckString)
-    //   .digest('hex');
-
-    // if (computedHash !== hash) {
-    //   console.log(computedHash, hash);
-    //   return { status: 'error', message: 'Authentication failed' };
-    // }
-
-    const userFromDB = await this.userService.findOrCreateUser(user as any);
-
-    const jwtToken = this.authService.generateToken(userFromDB);
-
-    return { message: 'Authenticated', token: jwtToken };
   }
 }
